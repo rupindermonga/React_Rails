2044dc4b7b41948f0d8450fac730d34c2851441b
Rupinder Monga <rupindermonga@gmail.com>
2019-09-12 11:01:18 +0530

    React Registration Component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Registration from "./auth/Registration";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <Registration />
+      </div>
+    );
+  }
+}
+
+export default Home;
