f8a9ba2f5e72a9038db3e3b3ec7dae69afa1c4a4
Rupinder Monga <rupindermonga@gmail.com>
2019-09-12 11:46:24 +0530

    Logged In state and Redirecting Users

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -37,7 +37,9 @@ class Registration extends Component {
         { withCredentials: true }
       )
       .then(response => {
-        console.log("registration res", response);
+        if (response.data.status === "created") {
+          this.props.handleSuccessfulAuth(response.data);
+        }
       })
       .catch(error => {
         console.log("registration error", error);
